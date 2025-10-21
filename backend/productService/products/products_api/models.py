fbfe695b0bb7daf42edeb044e4ab6f6e0cc57c42
DavitAndguladze <156723431+DavitAndguladze@users.noreply.github.com>
2025-10-21 15:08:11 -0500

    Fixing routing and bugs in django app category and porudcts and connect them together

--- backend/productService/products/products_api/models.py
+++ backend/productService/products/products_api/models.py
@@ -1,14 +1,10 @@
-from django.db import models
-<<<<<<< HEAD
-from category_api.models import Category  
-=======
->>>>>>> feat/products
+from django.db import models 
  
 class Product(models.Model):
     name = models.CharField(max_length=30)
     author = models.CharField(max_length=20)
     date_published = models.DateField()
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    category = models.ForeignKey("category_api.Category", on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
