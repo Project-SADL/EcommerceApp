7b3a317574905e75190292ae08ec620b6fa223da
DavitAndguladze <156723431+DavitAndguladze@users.noreply.github.com>
2025-10-21 14:41:10 -0500

    Modifing products_api and fixing routings to main products

--- backend/productService/products/products_api/models.py
+++ backend/productService/products/products_api/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-
-# Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-    
+ 
 class Product(models.Model):
     name = models.CharField(max_length=30)
     author = models.CharField(max_length=20)
     date_published = models.DateField()
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
     
     
 
