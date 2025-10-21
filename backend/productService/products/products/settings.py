86f4c1375d29a0e8094c8ee357f03db508620996
shenriquez01 <shenriquez250@gmail.com>
2025-10-21 18:22:03 -0500

    Create password for database root user.

--- backend/productService/products/products/settings.py
+++ backend/productService/products/products/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ecommerceweb_db',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'datuna123',
         'HOST': 'localhost',
         'PORT': '3306',
     }
