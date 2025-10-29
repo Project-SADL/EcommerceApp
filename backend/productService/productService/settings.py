cb8628f27839298c5df83b879f7e0439cbdd2321
DavitAndguladze <156723431+DavitAndguladze@users.noreply.github.com>
2025-10-29 17:17:47 -0500

    Add apps for the productService - products, books, giftcards, categories, productVariants

--- backend/productService/productService/settings.py
+++ backend/productService/productService/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'products',
+    'categories',
+    'books',
+    'giftCards',
+    'productVariants',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ backend/productService/productVariants/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProductvariantsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'productVariants'
